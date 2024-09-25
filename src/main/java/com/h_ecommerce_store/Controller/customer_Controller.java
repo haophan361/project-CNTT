ef54f2e4a6d72b0df77ec8b044013ba3674729ce
haophan361 <phuchao004@gmail.com>
2024-09-25 18:55:17 +0700

    fix store.html

--- src/main/java/com/h_ecommerce_store/Controller/customer_Controller.java
+++ src/main/java/com/h_ecommerce_store/Controller/customer_Controller.java
@@ -1,6 +1,14 @@
 package com.h_ecommerce_store.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class customer_Controller
 {
-
+    @GetMapping("/")
+    public String home()
+    {
+        return "web/store";
+    }
 }
