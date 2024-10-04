b1568817f8caf6b604e9cd209887fdbce08bfcd2
haophan361 <phuchao004@gmail.com>
2024-10-04 21:12:03 +0700

    fix login

--- src/main/java/com/h_ecommerce_store/Repository/Account_Repository.java
+++ src/main/java/com/h_ecommerce_store/Repository/Account_Repository.java
@@ -3,6 +3,7 @@ package com.h_ecommerce_store.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.h_ecommerce_store.Model.Accounts;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,4 +11,6 @@ public interface Account_Repository extends JpaRepository<Accounts,String>
 {
     @Query("SELECT a.username FROM Accounts a")
     List<String> findAllEmail();
+    @Query("SELECT a FROM Accounts  a WHERE a.username = :username")
+    Accounts findByUsername(@Param("username")String username);
 }
