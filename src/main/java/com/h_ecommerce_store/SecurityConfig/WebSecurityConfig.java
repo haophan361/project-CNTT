20ddabf66d12cc065a9642580ed670f91af7436b
haophan361 <phuchao004@gmail.com>
2024-10-04 21:12:03 +0700

    fix login

--- src/main/java/com/h_ecommerce_store/SecurityConfig/WebSecurityConfig.java
+++ src/main/java/com/h_ecommerce_store/SecurityConfig/WebSecurityConfig.java
@@ -4,56 +4,52 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import com.h_ecommerce_store.Service.account_Service;
+import org.springframework.security.web.SecurityFilterChain;
+
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig
-{
+public class WebSecurityConfig {
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
-    {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
+
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers(
                                 "/",
                                 "/web/**",
                                 "/js/**",
                                 "/css/**",
-                                "/fonts/**", "/img/**",
+                                "/fonts/**",
+                                "/img/**",
                                 "/screenshot/**").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/user/**").hasAnyRole("ADMIN", "USER")
                         .anyRequest().authenticated())
                 .formLogin(form -> form
                         .loginPage("/login")
-                        .defaultSuccessUrl("/", true)
+                        .defaultSuccessUrl("/user/home")
                         .permitAll())
                 .logout(logout -> logout
                         .logoutSuccessUrl("/")
                         .permitAll());
         return http.build();
     }
+
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception
     {
         return authenticationConfiguration.getAuthenticationManager();
     }
-    @Bean
-    public UserDetailsService userDetailsService()
-    {
-        return new account_Service();
-    }
+
     @Bean
     public PasswordEncoder passwordEncoder()
     {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/h_ecommerce_store/Service/account_Service.java
+++ src/main/java/com/h_ecommerce_store/Service/account_Service.java
@@ -4,47 +4,44 @@ import com.h_ecommerce_store.Model.Accounts;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.h_ecommerce_store.Repository.Account_Repository;
-import org.springframework.security.core.userdetails.UserDetails;
 import java.util.List;
-import java.util.Optional;
-
 @Service
 public class account_Service implements UserDetailsService
 {
     @Autowired
     Account_Repository account_repository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
+    public UserDetails loadUserByUsername(String username)
     {
-        Optional<Accounts> account=account_repository.findById(username);
-        if (account.isPresent())
-        {
-            List<SimpleGrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(account.get().getRole()));
-            return new User(account.get().getUsername(), account.get().getPassword(), authorities);
-        }
-        throw new UsernameNotFoundException("Tài khoản: " + username+" không tồn tại");
+        Accounts account=account_repository.findByUsername(username);
+        return new org.springframework.security.core.userdetails.User(
+                account.getUsername(),
+                account.getPassword(),
+                List.of(new SimpleGrantedAuthority(account.getRole()))
+        );
     }
     public Accounts getAccount(String username)
     {
-        Optional<Accounts> account=account_repository.findById(username);
-        return account.orElse(null);
+        return account_repository.findByUsername(username);
     }
     public Accounts insertAccount(Accounts account)
     {
+        String encodedPassword =passwordEncoder.encode(account.getPassword());
+        account.setPassword(encodedPassword);
         account.setRole("ROLE_USER");
         return account_repository.save(account);
     }
     public Accounts changePassword(String username, String newPassword)
     {
-        Optional<Accounts> account=account_repository.findById(username);
-        if(account.isPresent())
-        {
-            account.get().setPassword(newPassword);
-            return account_repository.save(account.get());
-        }
-        return null;
+        Accounts account=account_repository.findByUsername(username);
+        String encodedPassword =passwordEncoder.encode(newPassword);
+        account.setPassword(encodedPassword);
+        return account_repository.save(account);
     }
     public List<String> findAllEmail()
     {
